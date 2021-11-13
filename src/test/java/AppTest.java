753d88482edd0774c6a84e7834acb218749024bd
lanasergeeva <marikakopina@gmail.com>
2021-11-13 15:12:40 +0300

    настройка чекстайла

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,13 @@
 
 import org.junit.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
-public class AppTest  {
+public class AppTest {
     @Test
     public void whenSum() {
         App app = new App();
-        assertThat( app.sum(5), is(10));
+        assertThat(app.sum(5), is(10));
     }
 }
